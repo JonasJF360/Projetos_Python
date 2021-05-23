00a4f470f16c13dca7597f331af9fbfc027a4a2b
Jonas de Jesus Ferreira <48357110+JonasJF360@users.noreply.github.com>
2021-05-23 18:01:06 -0400

    Update Tabuada_interativa.py

--- Tabuada_interativa.py
+++ Tabuada_interativa.py
@@ -2,7 +2,7 @@ import re
 import random
 
 
-def is_float(val):
+def is_float(val):  # Verifica se o número é de ponto flutuante
     if isinstance(val, float):
         return True
     if not re.search(r'^-?[0-9]+\.[0-9]+$', val):
@@ -10,7 +10,7 @@ def is_float(val):
     return True
 
 
-def is_int(val):
+def is_int(val):  # Verifica se é um número inteiro
     if isinstance(val, int):
         return True
     if re.search(r'^-?[0-9]+$', val):
@@ -18,11 +18,18 @@ def is_int(val):
     return False
 
 
-def is_number(val):
+def is_number(val):  # Verifiva se é de fato um número
     return is_int(val) or is_float(val)
 
 
-# tabuada do 1 ao 10:
+"""
+Esse programa irá apresentar a tabuada do 1 ao 10 se você 
+digitar a letra 'T' ou a tabuada de algum número que for
+digitado ao invés de 'T'.
+Caso o valor digitado não seja nem um número e nem a letra
+'T' será apresentada uma menságem de erro e uma tabuada
+gerada aleatóriamente entre 1 e 10.
+"""
 def tabuada(x):
     print('-' * 12)
     for y in range(1, 11):
